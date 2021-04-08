0fc331bc4bf0214bc2f0d8b1e3e0383e9cafed39
Yashbhangdia <bhangdiayash3@gmail.com>
2021-04-08 13:14:53 +0530

    Created Slick Slider for testimonials

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,31 +1,56 @@
 import React from 'react';
 import Card from './cards';
 import './Styles/Testimonials.css'
+import Slider from "react-slick";
 
-function Testimonial()
-{
-    return(
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function Testimonial() {
+    const config = {
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: false
+    };
+    return (
         <div className="Testimonial">
             <div className="Heading pt-5">
                 <h1>Kind words from our clients</h1>
                 <p>What other people thought about the service provided by us</p>
             </div>
-            <div className="row mx-0">
-                <Card 
+            <div className="container">
+            <Slider {...config}>
+                <Card
                     name="Robert"
                     skill="Web Designer"
                     src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
                 />
-                <Card 
+                <Card
                     name="Stacy"
                     skill="Data Scientist"
                     src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDZ8fGZhY2UlMjBoYXBweXxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
                 />
-                <Card 
+                <Card
                     name="Peter"
                     skill="Human Resources"
                     src="https://images.unsplash.com/photo-1605857840732-188f2f08cb31?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTE3fHxmYWNlfGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
                 />
+                <Card
+                    name="Stacy"
+                    skill="Data Scientist"
+                    src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDZ8fGZhY2UlMjBoYXBweXxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+                />
+                <Card
+                    name="Peter"
+                    skill="Human Resources"
+                    src="https://images.unsplash.com/photo-1605857840732-188f2f08cb31?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTE3fHxmYWNlfGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+                />
+            </Slider>
             </div>
         </div>
     );
--- src/Components/cards.js
+++ src/Components/cards.js
@@ -4,20 +4,24 @@ import './Styles/cards.css';
 function card(props)
 {
     return (
-        <div className="Card col-md-4">
-            <img src={props.src} alt="Hello" className="profile col-md-5"></img>
-            <div className="row pt-4">
-                <div className="Name col-md-8">
+        <div className="col-md-12">
+        <div className="MyCard">
+            <div className="col-md-4">
+            <img src={props.src} alt="Hello" className="profile"></img>
+            </div>
+            <div className="row pt-4 mx-0">
+                <div className="Name col-md-10">
                   <h3>{props.name}<span>{props.skill}</span></h3>
                 </div>
             </div>
-            <div className="row pt-2 pb-4">
+            <div className="row pt-2 pb-4 mx-0">
                 <div className="col-md-12">
                     <p className="testimonial">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
                 </div>
             </div>
+        </div>
         </div>
     );
 }
